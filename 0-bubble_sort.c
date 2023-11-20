3139f9be373d5ec2b7d1e9bc429c5555973eb2e9
mohrahman <mohrahman558@gmail.com>
2023-11-20 21:30:46 +0000

    bubble sort algo

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp, swp = 1;
+	size_t i = size, j;
+
+	if (array == NULL || size <= 1)
+		return;
+
+	while (swp)
+	{
+		swp = 0;
+		for (j = 0; j < i - 1; j ++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swp = 1;
+				print_array(array, size);
+			}
+		}
+		i--;
+	}
+}
