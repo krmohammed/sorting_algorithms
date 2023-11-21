23cf63410e2fe008852041ed4b18915ea767b066
mohrahman <mohrahman558@gmail.com>
2023-11-21 11:28:35 +0000

    tests for selection sort

---
+++ 2-main2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int array[] = {129, 97, 54, 73, 44, 164, 175, 195, 156, 40, 174, 126, 159, 75, 59};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    selection_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
+}
