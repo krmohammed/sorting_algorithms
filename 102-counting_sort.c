91b9cd8e374a14577829adb79453b0d1be3f0909
mohrahman <mohrahman558@gmail.com>
2023-11-22 03:07:43 +0000

    improvement

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,15 +10,16 @@
 void counting_sort(int *array, size_t size)
 {
 	size_t i;
-	int j, k;
-	int *count, max;
+	int j, *count, *opt, max;
 
 	max = array[0];
 	for (i = 0; i < size; i++)
-	{
 		if (array[i] > max)
 			max = array[i];
-	}
+
+	opt = malloc(sizeof(int) * size);
+	if (opt == NULL)
+		return;
 
 	count = malloc(sizeof(int) * (max + 1));
 	if (count == NULL)
@@ -30,13 +31,20 @@ void counting_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 		count[array[i]]++;
 
+	for (i = 1; i <= (size_t)max; i++)
+		count[i] += count[i - 1];
+
 	print_array((const int *)count, max + 1);
 
-	for (j = 0, k = 0; j <= max; j++)
+	for (i = 0; i < size; i++)
 	{
-		while (count[j]-- > 0)
-			array[k++] = j;
+		opt[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
 	}
 
+	for (i = 0; i < size; i++)
+		array[i] = opt[i];
+
 	free(count);
+	free(opt);
 }
